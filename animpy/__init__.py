a61f5d0f75829d7ca0fc5e5be9c796220ab84c5f
Martin Uribe <clamytoe@gmail.com>
2018-09-21 22:15:22 -0500

    Cleaned up the imports

--- animpy/__init__.py
+++ animpy/__init__.py
@@ -1,3 +1,9 @@
+from .animpy import (cleanup_reviews, clear_screen, display_review_choice,
+                     display_reviews, display_summary, exit_message,
+                     scrape_details, scrape_hits, scrape_reviews, search,
+                     search_divs, LINES, WIDTH)
+from .main import cli, display_all_hits, display_error
+
 __author__ = 'Martin Uribe'
 __email__ = 'clamytoe@gmail.com'
-__version__ = '0.3.0'
+__version__ = '0.4.0'
