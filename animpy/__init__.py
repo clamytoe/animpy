207044dbcce1105fa3a5be027a5a3e293c8ed067
Martin Uribe <clamytoe@gmail.com>
2018-09-21 22:19:00 -0500

    Updated imports with the new get_soup() function

--- animpy/__init__.py
+++ animpy/__init__.py
@@ -1,7 +1,7 @@
 from .animpy import (cleanup_reviews, clear_screen, display_review_choice,
                      display_reviews, display_summary, exit_message,
-                     scrape_details, scrape_hits, scrape_reviews, search,
-                     search_divs, LINES, WIDTH)
+                     get_soup, scrape_details, scrape_hits, scrape_reviews,
+                     search, search_divs, LINES, WIDTH)
 from .main import cli, display_all_hits, display_error
 
 __author__ = 'Martin Uribe'
